423d8b30a975fc605a77dc5ebe1074a3613c5c29
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-04-05 12:57:27 -0400

    2c is done

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ const tick = new Audio('sounds/tick.mp3');
 
 // Setup 'tock' sound
 const tock = new Audio ('sounds/tock.mp3')
+const kick = new Audio ('sounds/kick-drum.mp3')
+const snare = new Audio ('sounds/snare-drum.mp3')
+const hi = new Audio ('sounds/hi-hat.mp3')
 
 let num=0
 
 // This function is called every 600ms
 function update() {
     const metronome = document.querySelector('#metronome')
+    const kickDrum = document.querySelector('#kick-drum')
+    const snareDrum = document.querySelector('#snare-drum')
+    const hiHat = document.querySelector('#hi-hat')
+
     if (metronome.checked){
         const count = document.querySelector('#count')
         count.innerText = num
@@ -18,6 +25,12 @@ function update() {
             tick.play();
         }
         num++
+    } else if(kickDrum.checked){
+        kick.play()
+    } else if(snareDrum.checked){
+        snare.play()
+    } else if(hiHat.checked){
+        hi.play()
     }
     
 
